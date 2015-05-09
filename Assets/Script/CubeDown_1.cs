78a743c18fcec098a067c337b5d4499d8eb8e3c8
gushenzhou <gushenzhou56789@126.com>
2015-05-09 17:10:29 +0800

    creat box and destory box

---
+++ Assets/Script/CubeDown_1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeDown_1 : MonoBehaviour {
+
+	public GameObject go;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			GameObject obj=(GameObject)GameObject.Instantiate(go,gameObject.transform.position,gameObject.transform.rotation);
+			obj.renderer.material.color=Color.red;
+		}
+	}
+}
